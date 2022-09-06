c9ad569b63d4e9fb389d8d912fa431f3e340f7e6
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-09-06 05:26:39 -0700

    check code snapshot, move instantiate to bottom

--- server.js
+++ server.js
@@ -3,54 +3,67 @@ const express = require('express');
 
 //set port to environmental variable
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// instantiate: make the server listen to the app
-const app = express(); 
-app.listen(PORT, () => {
-    console.log('API server now on port ${PORT}!');
-});
-
-// create route front end can use to request data from
+//create route the font end can use to get data from
 const { animals } = require('./data/animals');
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    const result = findById(req.params.id, animals);
-    res.json(animals);
-});
 
 function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    // animalsArray saved as filteredResults 
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        //save personalityTraits as a dedicated array
-        //if personalityTraits is a string, place it into a new array and save
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = [query.personalityTraits];
-        }
-        //loop through each trait in the personalityTraits array
-        personalityTraitsArray.forEach(trait => {
-            //check the trait against each animal in the filteredResults array (which starts as a copy of the animalsArray) but then becomes an array matching the query
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
+  let personalityTraitsArray = [];
+      // animalsArray saved as filteredResults 
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    //save personalityTraits as a dedicated array
+    //if personalityTraits is a string, place it into a new array and save
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
     }
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-};
+//loop through each trait in the personalityTraits array
+    personalityTraitsArray.forEach(trait => {
+    //check the trait against each animal in the filteredResults array (which starts as a copy of the animalsArray) but then becomes an array matching the query
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  return filteredResults;
+}
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+app.get('/api/animals', (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+// instantiate: make the server listen on a port
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
 
