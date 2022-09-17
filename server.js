4f05c1922efd55429855119a3dd34accab44900e
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-09-16 22:28:35 -0700

    finished mvp

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 //install express
 const express = require('express');
 
 //set port to environmental variable
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming JSON data
+app.use(express.json());
 
 //create route the font end can use to get data from
 const { animals } = require('./data/animals');
@@ -40,9 +47,20 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+//accept POST route's req.body value
 function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(_dirname, './data/animals.json'),
+    JSON.stringify({animals: animalsArray}, null, 2)
+  ); 
+  return animal; 
 }
 
 app.get('/api/animals', (req, res) => {
@@ -62,8 +80,39 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+// req.body is where our incoming content will be
+//set id based on what the next index of the array will be
+req.body.id = animals.length.toString();
+
+// if any data in req.body is incorrect, send 400 error back
+if (!validateAnimal(req.body)) {
+  res.status(400).send('The animal is not properly formatted.');
+} else {
+  //add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+  res.json(req.body);
+}
+});
+
 // instantiate: make the server listen on a port
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false; 
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+};
 
